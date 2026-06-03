0f641fa930542f77cc950f54dda692151e5b3d99
JDibena <jdibena@gmail.com>
2026-06-03 17:54:51 +0100

    Style: added a scroll-to-top button.

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const yearSpan = document.getElementById('current-year');
 yearSpan.textContent = new Date().getFullYear();
+
+const backToTopBtn = document.getElementById("backToTop");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+};
+
+backToTopBtn.onclick = function() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+};
